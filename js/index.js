04e873351f08db2dfb79a78a94be872785f2f8d3
aliakakis <aliakakis@gmail.com>
2017-04-20 21:11:11 +0300

    Changes

--- js/index.js
+++ js/index.js
@@ -1,16 +1,37 @@
+/**
+ * Bootstrap application
+ */
 import AppStart from './app-start/app-start';
 
+/**
+ * Common components for entire app
+ */
 import AppHeader from './common/components/app-header';
+
+/**
+ * Routes
+ */
 import Home from './pages/home-page/home-page';
 import SignUp from './pages/signup-page/signup-page';
 import Login from './pages/login-page/login-page';
 
+/**
+ * Bootstrap application component
+ */
+ko.components.register('app-start', AppStart);
+
+/**
+ * Common components
+ */
 ko.components.register('app-header', AppHeader);
+
+/**
+ * Routes components
+ */
 ko.components.register('home-page', Home);
 ko.components.register('signup-page', SignUp);
 ko.components.register('login-page', Login);
 
-ko.components.register('app-start', AppStart);
 
 ko.applyBindings();
 
